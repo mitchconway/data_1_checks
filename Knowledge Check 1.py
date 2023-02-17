4efe982f7fcced32b5dd7c7850853380a18cd965
mitchconway <122239314+mitchconway@users.noreply.github.com>
2023-02-16 20:45:27 -0500

    knowledge check finished

---
+++ Knowledge Check 1.py
@@ -0,0 +1,14 @@
+#statement that prints "hello world!"
+print('hello world!')
+#list with several values
+boardgames = ['Scythe', 'Terraforming Mars', 'Root', 'Frosthaven', 'Spirit Island', 'Nemesis']
+#print from list
+print(boardgames[3])
+#create a dictionary with 2 keys and 2 values
+gamenight = {'Scythe':'Monday', 'Terraforming Mars':'Tuesday'}
+#print the value
+print(gamenight['Terraforming Mars'])
+#create a tuple with 4 values
+videogames = ('Pokemon', 'Zelda', 'Cult of the Lamb', 'Mario Kart')
+#print a value from the tuple
+print(videogames[3])
